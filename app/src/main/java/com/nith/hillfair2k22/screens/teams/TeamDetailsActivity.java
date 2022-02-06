8d33bc89a2025577742e6abe927455f207dc92ec
skdbsp123 <skdbsp123@gmail.com>
2022-02-06 13:11:41 +0530

    added package structures

---
+++ app/src/main/java/com/nith/hillfair2k22/screens/teams/TeamDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.nith.hillfair2k22.screens.teams;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nith.hillfair2k22.R;
+
+public class TeamDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_details);
+    }
+}
